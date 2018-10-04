f610c3d6ebe8821fbe7f806b31d032cb386c22eb
marceloleite-acras <marcelo.leite@acras.com.br>
2018-10-04 16:26:07 -0300

    MDe - adicionados exemplos documentação em ruby

---
+++ mde/manifestar.rb
@@ -0,0 +1,53 @@
+# encoding: UTF-8
+
+require "net/http"
+require "net/https"
+require "json"
+
+# token enviado pelo suporte
+token = "codigo_alfanumerico_token"
+
+chave = "chave_de_identificacao_da_NFe"
+
+# endereço da api que deve ser usado conforme o ambiente: produção ou homologação
+servidor_producao = "https://api.focusnfe.com.br/"
+servidor_homologacao = "http://homologacao.acrasnfe.acras.com.br/"
+
+# no caso do ambiente de envio ser em produção, utilizar servidor_producao
+url_envio = servidor_homologacao + "v2/nfes_recebidas/" + chave + "/manifesto"
+
+# altere os campos conforme a nota que será enviada
+tipo_manifestacao = {  
+  tipo: "nao_realizada",
+}
+
+# caso escolha o tipo "nao_realizada", é preciso informar o campo/chave "justificativa"
+# justificativa: "Informe aqui a sua justificativa do motivo da não realização da operação."
+
+# criamos uma objeto uri para envio da nota
+uri = URI(url_envio)
+
+# também criamos um objeto da classe HTTP a partir do host da uri
+http = Net::HTTP.new(uri.hostname, uri.port)
+
+# aqui criamos um objeto da classe Post a partir da uri de requisição
+requisicao = Net::HTTP::Post.new(uri.request_uri)
+
+# adicionando o token à requisição
+requisicao.basic_auth(token, '')
+
+# convertemos a hash de justificativa do cancelamento para o formato JSON e adicionamos ao corpo da requisição
+requisicao.body = tipo_manifestacao.to_json
+
+# no envio de notas em produção, é necessário utilizar o protocolo ssl
+# para isso, basta retirar o comentário da linha abaixo
+# http.use_ssl = true
+
+# aqui enviamos a requisição ao servidor e obtemos a resposta
+resposta = http.request(requisicao)
+
+# imprimindo o código HTTP da resposta
+puts "Código retornado pela requisição: " + resposta.code
+
+# imprimindo o corpo da resposta
+puts "Corpo da resposta: " + resposta.body
